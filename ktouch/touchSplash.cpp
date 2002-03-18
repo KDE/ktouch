887d64cc89fd563c44364ef681241f564289c268
George Staikos <staikos@kde.org>
2002-03-18 08:45:30 +0000

    get the splash on the right screen.
    svn path=/trunk/kdeedu/ktouch/; revision=143598

--- ktouch/touchSplash.cpp
+++ ktouch/touchSplash.cpp
@@ -2,6 +2,7 @@
 #include "touchSplash.h"
 #include <kapplication.h>
 #include <kstandarddirs.h>
+#include <qcursor.h>
 
 TouchSplash::TouchSplash(QWidget * parent, const char *name)
 	   : QWidget(parent,name, WStyle_NoBorderEx | WStyle_Customize | WDestructiveClose ), m_bReadyToHide( false )
@@ -9,7 +10,7 @@ TouchSplash::TouchSplash(QWidget * parent, const char *name)
   QPixmap pm;
   pm.load(locate("appdata", "splash.png"));
   setBackgroundPixmap(pm);
-  int screen = QApplication::desktop()->screenNumber(parent);
+  int screen = QApplication::desktop()->screenNumber(QCursor::pos());
   QRect geom = QApplication::desktop()->screenGeometry(screen);
   setGeometry(geom.width()/2-pm.width()/2 + geom.x(), 
               geom.height()/2-pm.height()/2 + geom.y(), 
