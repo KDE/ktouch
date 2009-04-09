91cecc9b7d3a8b4289e07893f2aebf8bc67a9384
Andrew Coles <andrew.i.coles@googlemail.com>
2009-04-09 20:13:39 +0000

    SVN_SILENT
    Added line to Messages.sh to fetch strings from the .kcfg file too.
    
    
    svn path=/trunk/KDE/kdeedu/ktouch/; revision=951633

--- Messages.sh
+++ Messages.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 $EXTRACTRC `find src -name '*.rc'` >> rc.cpp
 $EXTRACTRC `find src/ui -name *.ui` >> rc.cpp
+$EXTRACTRC `find src -name '*.kcfg'` >> rc.cpp
 $EXTRACTRC --tag=Title --context "Lesson Name" `find training -name *.ktouch.xml` >> rc.cpp
 $EXTRACTRC --tag=Title --context "Keyboard Layout Name" `find keyboards -name *.keyboard.xml` >> rc.cpp
 $XGETTEXT rc.cpp src/*.cpp -o $podir/ktouch.pot
