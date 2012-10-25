79941a59710c515dbcdf17435040a730e85979c6
Sebastian Gottfried <sebastiangottfried@web.de>
2012-10-25 19:23:11 +0200

    fix message extraction
    Was missing for all QML files and all C++ files in subdirectories.

--- Messages.sh
+++ Messages.sh
@@ -2,5 +2,6 @@
 $EXTRACTRC `find src -name '*.rc'` >> rc.cpp
 $EXTRACTRC `find src/ui -name *.ui` >> rc.cpp
 $EXTRACTRC `find src -name '*.kcfg'` >> rc.cpp
-$XGETTEXT rc.cpp src/*.cpp -o $podir/ktouch.pot
+$XGETTEXT rc.cpp `find src/ -name \*.qml -or -name \*.cpp` -o $podir/ktouch.pot
+
 
