cbb065ade17493f563d0e47a2e699e4ab69c2c5d
Andreas Cord-Landwehr <cordlandwehr@kde.org>
2016-10-01 20:13:25 +0200

    Unload QML engine when layout object gets deleted
    This allows the QML items to assume that the layout object always exists.

--- src/editor/keyboardlayouteditor.cpp
+++ src/editor/keyboardlayouteditor.cpp
@@ -69,6 +69,11 @@ KeyboardLayoutEditor::KeyboardLayoutEditor(QWidget* parent):
 
 }
 
+KeyboardLayoutEditor::~KeyboardLayoutEditor()
+{
+    m_view->setSource(QUrl());
+}
+
 void KeyboardLayoutEditor::openKeyboardLayout(DataIndexKeyboardLayout* dataIndexKeyboardLayout)
 {
     DataAccess dataAccess;
