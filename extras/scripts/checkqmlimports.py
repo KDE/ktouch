171f96e3d448a39f1f1f20f17b4e6b5ea6450aed
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-03-08 22:02:55 +0100

    Convert license headers to SPDX

--- extras/scripts/checkqmlimports.py
+++ extras/scripts/checkqmlimports.py
@@ -1,20 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-#  Copyright 2017  Sebastian Gottfried <sebastian.gottfried@posteo.de>
+#  SPDX-FileCopyrightText: 2017 Sebastian Gottfried <sebastian.gottfried@posteo.de>
 #
-#  This program is free software; you can redistribute it and/or
-#  modify it under the terms of the GNU General Public License as
-#  published by the Free Software Foundation; either version 2 of
-#  the License, or (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#  SPDX-License-Identifier: GPL-2.0-or-later
 #
 
 import argparse
